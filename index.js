45c817efcd311710e118d86e64a278fadc1fdcc1
Shabex <shabsibrah@gmail.com>
2022-06-07 09:39:06 +0300

    commit tree d14e22dc7e7bc6beb3972143770246e3c8f97164

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks (someValue) {
+    if (someValue<42){
+        return(42-someValue)
+    }else {
+        return (someValue-42)
+    }
+
+}
+function distanceFromHqInFeet (someValue){
+    return distanceFromHqInBlocks (someValue)*264;
+}
+
+function distanceTravelledInFeet (start, destination){
+    return Math.abs((destination-start)*264)
+}
+
+function calculatesFarePrice(start,destination) {
+    var distance = destination-start;
+    const feet = Math.abs(distance)*264;
+    if (Math.abs(distance)===1){
+        return 0;
+    } 
+    if (feet<=2000){
+    return (Math.abs (feet)-400)*0.02;
+    } 
+    else if (feet<=2500){
+        return 25;
+    } 
+    else if (feet>2500){
+        return "cannot travel that far"
+    }
+
+}
